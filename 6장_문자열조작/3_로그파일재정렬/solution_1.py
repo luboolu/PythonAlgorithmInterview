c375c1f2f780b9115a3ece2a61e0598a78f34b42
luboolu <kjjy98@naver.com>
2021-09-08 22:57:41 +0900

    string

---
+++ 6장_문자열조작/3_로그파일재정렬/solution_1.py
@@ -0,0 +1,15 @@
+#리트코드 937 Reorder Log File
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+
+sol = Solution()
+print(sol.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
--- 6장_문자열조작/3_로그파일재정렬/solution_my.py
+++ 6장_문자열조작/3_로그파일재정렬/solution_my.py
@@ -1,13 +1,9 @@
 #리트코드 937 Reorder Log File
-
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        # logs = sorted(logs)
-        answer = []
         logs_str = []
         logs_num = []
 
-        print(logs)
         for l in logs:
             l_split = l.split(" ")
             identifier = l_split.pop(0)
@@ -18,10 +14,9 @@ class Solution:
             else:
                 logs_num.append(l)
 
-            # result.sort(key=lambda x: x["weight"], reverse=True)
+        logs_str.sort(key=lambda x: (x.split()[1:], x.split()[0]))
 
         return logs_str + logs_num
-
 
 sol = Solution()
 print(sol.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
