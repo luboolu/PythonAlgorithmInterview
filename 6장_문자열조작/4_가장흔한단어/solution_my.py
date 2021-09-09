982ffad78fc850061670eb6aa4d650419d610eae
luboolu <kjjy98@naver.com>
2021-09-09 22:29:25 +0900

    string

--- 6장_문자열조작/4_가장흔한단어/solution_my.py
+++ 6장_문자열조작/4_가장흔한단어/solution_my.py
@@ -2,7 +2,24 @@
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        word = []
-        for
+        words = paragraph.split(" ")
+        print(words)
+        dic = []
 
+        for i in range(len(banned)):
+            banned[i] = banned[i].lower()
 
+        for word in words:
+            tmp = ""
+            for w in word:
+                if w.isalpha():
+                    tmp += w.lower()
+
+            if tmp not in banned:
+                dic.append(tmp)
+
+        cnt = []
+        for d in dic:
+            cnt.append(dic.count(d))
+
+        return dic[cnt.index(max(cnt))]
