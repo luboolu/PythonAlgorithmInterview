3ed869a6ca65aed593cc8921f333f1860f6a9f9e
luboolu <kjjy98@naver.com>
2021-09-14 23:34:53 +0900

    string

--- 6장_문자열조작/6_가장긴팰린드롬부분문자열/solution_my.py
+++ 6장_문자열조작/6_가장긴팰린드롬부분문자열/solution_my.py
@@ -1,2 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        can = []
+
+        for i, a in enumerate(s):
+            for j, b in enumerate(s):
+                slc = s[i: j + 1]
+
+                if slc != "":
+                    can.append(slc.lower())
+
+        palindrome = []
+        for c in can:
+            reverse = ""
+            is_palindrome = False
+
+            for i in range(len(c)):
+                reverse += c[-1 - i]
+
+            if c == reverse:
+                palindrome.append(c)
+
+        palindrome.sort(key=lambda x: len(x), reverse=True)
+
+        return palindrome[0]
