bce934f2daaec15c75cdf483d85da492750d5bc0
luboolu <kjjy98@naver.com>
2021-09-18 22:07:06 +0900

    string

--- 7장_배열/8_빗물트래핑/solution_my.py
+++ 7장_배열/8_빗물트래핑/solution_my.py
@@ -7,9 +7,9 @@ class Solution:
         ## i - a <= i < i + a 인 경우를 찾아야함
         ##이런 경우를 찾는다면 answer += a
 
-        left, right = 0, len(height) - 1
-        while not left == right:
-            if height[left]
+        max_height = max(height)
+
+
 
 sol = Solution()
 print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
