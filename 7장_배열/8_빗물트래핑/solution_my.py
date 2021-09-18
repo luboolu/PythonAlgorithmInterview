d4ae7f25a73e56d1b63ae20f02b06f817d060a20
luboolu <kjjy98@naver.com>
2021-09-18 19:33:57 +0900

    string

--- 7장_배열/8_빗물트래핑/solution_my.py
+++ 7장_배열/8_빗물트래핑/solution_my.py
@@ -2,3 +2,14 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        print(height)
+        ##i - a / i / i + a
+        ## i - a <= i < i + a 인 경우를 찾아야함
+        ##이런 경우를 찾는다면 answer += a
+
+        left, right = 0, len(height) - 1
+        while not left == right:
+            if height[left]
+
+sol = Solution()
+print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
