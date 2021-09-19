575bf990321af0be6dd3bb4a1da628f672dfd446
luboolu <kjjy98@naver.com>
2021-09-19 13:26:30 +0900

    string

--- 7장_배열/9_세수의합/solution_my.py
+++ 7장_배열/9_세수의합/solution_my.py
@@ -3,17 +3,19 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         answer = []
+        #nums.sort()
         for i, a in enumerate(nums):
             tmp_answer = [a]
-            for j in range(i, len(nums)):
-                tmp_answer.append(nums[j])
+            for j in range(i + 1, len(nums)):
+                for k in range(i + 1, len(nums)):
+                    tmp_answer.append(nums[k])
 
-                if len(tmp_answer) == 3:
-                    if sum(tmp_answer) == 0:
-                        answer.append(tmp_answer)
+                    if len(tmp_answer) == 3:
+                        print(tmp_answer)
+                        if sum(tmp_answer) == 0:
+                            answer.append(tmp_answer)
                         break
-                    break
-            tmp_answer.clear()
+                tmp_answer.clear()
 
         return answer
 
