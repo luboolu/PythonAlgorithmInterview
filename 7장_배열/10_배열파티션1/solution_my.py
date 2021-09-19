974b81881264c0ffd7facb0e7a5b0d1503f6d619
luboolu <kjjy98@naver.com>
2021-09-19 19:26:51 +0900

    string

--- 7장_배열/10_배열파티션1/solution_my.py
+++ 7장_배열/10_배열파티션1/solution_my.py
@@ -5,16 +5,14 @@ class Solution:
         answer = 0
         n = len(nums) // 2
         nums.sort()
-        nums = [str(n) for n in nums]
-        nums = "".join(nums)
-        split_num = [nums[i:i+2] for i in range(0, len(nums), 2)]
 
-        print(split_num)
+        split_num = [nums[i:i + 2] for i in range(0, len(nums), 2)]
 
-        for i in range(n):
-            answer += min(int(split_num[i][0]), int(split_num[i][1]))
+        for i in range(len(nums) // 2):
+            answer += min(split_num[i][0], split_num[i][1])
 
         return answer
 
 sol = Solution()
 print(sol.arrayPairSum([6214, -2290, 2833, -7908]))
+print(sol.arrayPairSum([1,4,3,2]))
