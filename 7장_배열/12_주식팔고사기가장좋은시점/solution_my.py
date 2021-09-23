11560825f0f9ef92eb7f8e59dfa484d2d50ca724
luboolu <kjjy98@naver.com>
2021-09-23 21:02:42 +0900

    array

--- 7장_배열/12_주식팔고사기가장좋은시점/solution_my.py
+++ 7장_배열/12_주식팔고사기가장좋은시점/solution_my.py
@@ -4,6 +4,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #한번의 거래로(매수 1번, 매도 1) 낼 수 있는 최대 이익을 return번
         #최소일 때 사서, 최대일 때 팔아야 함(but, index(최소) < index(최대))
+        #TIMEOUT
         if len(prices) == 1:
             return 0
 
--- 7장_배열/12_주식팔고사기가장좋은시점/solution_my2.py
+++ 7장_배열/12_주식팔고사기가장좋은시점/solution_my2.py
@@ -4,16 +4,25 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #한번의 거래로(매수 1번, 매도 1) 낼 수 있는 최대 이익을 return번
         #최소일 때 사서, 최대일 때 팔아야 함(but, index(최소) < index(최대))
-        profit = [0]
-        min_day = prices.index(min(prices))
+        #최대인 지점과 profit이 최대인 지점이 항상 일치하는 것은 아니다.-틀림
+        if len(prices) == 1:
+            return 0
 
-        for i in range(1, len(prices)):
-            if i > min_day:
-                profit.append(prices[i] - prices[min_day])
+        profit = [0]
+        re_prices = prices[:0:-1]
+        max_day = len(prices) - re_prices.index(max(re_prices)) - 1
+
+        for i in range(len(prices)):
+            if i < max_day:
+                profit.append(prices[max_day] - prices[i])
 
         return max(profit)
 
 sol = Solution()
-print(sol.maxProfit([7, 1, 5, 3, 6, 4]))
-print(sol.maxProfit([2, 4, 1]))
-print(sol.maxProfit([7, 6, 4, 3, 1]))
+# print(sol.maxProfit([7, 1, 5, 3, 6, 4]))
+# print(sol.maxProfit([2, 4, 1]))
+# print(sol.maxProfit([7, 6, 4, 3, 1]))
+# print(sol.maxProfit([3, 2, 6, 5, 0, 3]))
+#print(sol.maxProfit([2, 1, 2, 0, 1, 2]))
+#print(sol.maxProfit([0,1,2,3,4,5]))
+print(sol.maxProfit([3,3,5,0,0,3,1,4]))
