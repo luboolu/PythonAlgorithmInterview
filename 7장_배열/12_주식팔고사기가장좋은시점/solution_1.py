ef57496ca09a6c3a4334c45848f89ce6db1735a7
luboolu <kjjy98@naver.com>
2021-09-23 21:03:28 +0900

    array

---
+++ 7장_배열/12_주식팔고사기가장좋은시점/solution_1.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        for p in prices:
+
+
+sol = Solution()
+print(sol.maxProfit([3,3,5,0,0,3,1,4]))
