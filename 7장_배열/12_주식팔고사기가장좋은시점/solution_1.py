89e264b129720c938efcf2cb72b12a929e0158eb
luboolu <kjjy98@naver.com>
2021-09-23 21:09:53 +0900

    array

--- 7장_배열/12_주식팔고사기가장좋은시점/solution_1.py
+++ 7장_배열/12_주식팔고사기가장좋은시점/solution_1.py
@@ -2,9 +2,14 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        profit = 0
 
         for p in prices:
+            min_price = min(min_price, p)
+            profit = max(p - min_price, profit)
 
+        return profit
 
 sol = Solution()
 print(sol.maxProfit([3,3,5,0,0,3,1,4]))
