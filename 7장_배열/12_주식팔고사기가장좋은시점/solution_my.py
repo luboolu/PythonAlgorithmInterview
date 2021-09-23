46b21a7216cb2a2ece433ba868047b91584498a1
luboolu <kjjy98@naver.com>
2021-09-23 19:04:05 +0900

    array

--- 7장_배열/12_주식팔고사기가장좋은시점/solution_my.py
+++ 7장_배열/12_주식팔고사기가장좋은시점/solution_my.py
@@ -4,15 +4,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #한번의 거래로(매수 1번, 매도 1) 낼 수 있는 최대 이익을 return번
         #최소일 때 사서, 최대일 때 팔아야 함(but, index(최소) < index(최대))
-        max_day = prices.index(max(prices[1:]))
-        min_day = prices.index(min(prices))
 
-        if min_day < max_day:
-            return prices[max_day] - prices[min_day]
+        if len(prices) == 1:
+            return 0
+
+        max_day = prices.index(max(prices[1:]))
+        min_day = prices.index(min(prices[:-1]))
+        print(min_day, max_day)
+        profit = prices[max_day] - prices[min_day]
+        if min_day < max_day and profit > 0:
+            return profit
         else:
             return 0
 
 
 sol = Solution()
 #print(sol.maxProfit([7, 1, 5, 3, 6, 4]))
-print(sol.maxProfit([2, 4, 1]))
+print(sol.maxProfit([2, 1]))
