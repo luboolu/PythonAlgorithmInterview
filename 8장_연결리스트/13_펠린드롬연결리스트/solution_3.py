293430f6bc5d32225b7c2a1485c4190aecb7b4cd
luboolu <kjjy98@naver.com>
2021-09-24 16:43:00 +0900

    linked list

--- 8장_연결리스트/13_펠린드롬연결리스트/solution_3.py
+++ 8장_연결리스트/13_펠린드롬연결리스트/solution_3.py
@@ -8,22 +8,20 @@ import collections
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # 데크 자료형 선언
-        q: Deque = collections.deque()
+        rev = None
+        slow = fast = head
+        #런너를 이용해 역순 연결 리스트 구성
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
 
-        if not head:
-            return True
+        if fast:
+            slow = slow.next
 
-        node = head
-        while node is not None:
-            q.append(node.val)
-            node = node.next
-
-        while len(q) > 1:
-            if q.popleft() != q.pop():
-                return False
-
-        return True
+        #펠린드롬 여부 확인
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
 
 
 sol = Solution()
