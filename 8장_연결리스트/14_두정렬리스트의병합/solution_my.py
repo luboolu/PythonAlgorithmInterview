8ecbc4ec636c1573f45f51bc26b58b6da4018e52
luboolu <kjjy98@naver.com>
2021-09-24 16:46:58 +0900

    linked list

---
+++ 8장_연결리스트/14_두정렬리스트의병합/solution_my.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        q1: List = []
+        q2: List = []
+
+        node1 = l1
+        node2 = l2
+        # 리스트 변환
+        while node1 is not None:
+            q1.append(node1.val)
+            node1 = node1.next
+
+        while node2 is not None:
+            q2.append(node2.val)
+            node2 = node2.next
+
+        print(q1)
+        print(q2)
