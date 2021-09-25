94f52122cf08d19ef2f17fb1845f73aa701524a3
luboolu <kjjy98@naver.com>
2021-09-25 19:40:01 +0900

    linked list

--- 8장_연결리스트/linkedlist.py
+++ 8장_연결리스트/linkedlist.py
@@ -7,12 +7,14 @@ class ListNode:
         self.val = val
         self.next = next
 
-#node에 데이터 추
+#node에 데이터 추가
 a1 = ListNode("a1")
 a2 = ListNode("a2")
+a3 = ListNode("a3")
 
 #각 노드를 연결
 a1.next = a2
+a2.next = a3
 
 print(a1.val)
 print(a1.next.val)
