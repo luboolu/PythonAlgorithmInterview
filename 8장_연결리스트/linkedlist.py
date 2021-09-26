5e603ce3e351317c9e4ba60a791a13a16d970acb
luboolu <kjjy98@naver.com>
2021-09-26 20:20:14 +0900

    linked list

--- 8장_연결리스트/linkedlist.py
+++ 8장_연결리스트/linkedlist.py
@@ -11,13 +11,17 @@ class ListNode:
 a1 = ListNode("a1")
 a2 = ListNode("a2")
 a3 = ListNode("a3")
+a4 = ListNode("a4")
 
 #각 노드를 연결
 a1.next = a2
 a2.next = a3
+a3.next = a4
 
 print(a1.val)
 print(a1.next.val)
 
-
-
+head = a1
+while head is not None:
+    print(head.val)
+    head = head.next
