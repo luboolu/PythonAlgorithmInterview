eb3b04f7f950e81fcc907ae82dedef077692cac3
luboolu <kjjy98@naver.com>
2021-09-27 23:46:26 +0900

    linked list

--- 8장_연결리스트/16_두수의덧셈/solution_my.py
+++ 8장_연결리스트/16_두수의덧셈/solution_my.py
@@ -5,3 +5,10 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        answer = ListNode()
+
+        while l1:
+            s = ListNode(l1.val + l2.val)
+            print(s)
+            l1 = l1.next
+            l2 = l2.next
